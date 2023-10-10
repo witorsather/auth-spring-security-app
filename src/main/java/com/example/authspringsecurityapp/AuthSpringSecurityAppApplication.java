afce29661f67960b9572862e57b35ea5d0d98df8
Witor Sather <satherws@gmail.com>
2023-10-10 07:38:03 -0300

    :tada: Commit inicial.

---
+++ src/main/java/com/example/authspringsecurityapp/AuthSpringSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.authspringsecurityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringSecurityAppApplication.class, args);
+	}
+
+}
